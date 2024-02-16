b003b5f084ce5119af0950f62f9133bebace7359
Smoshi0521 <98013167+Smoshi0521@users.noreply.github.com>
2024-02-16 19:57:34 +0800

    Working Main Page
    Working Main Page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,10 +2,43 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full bg-transparent fixed top-0 z-50 p-2'>
+    <div className='w-full bg-transparent fixed top-0 z-50 p-2 flex justify-between'>
       <div className='w-[100px] bg- rounded-xl p-2'>
         <img src='/assets/Logo/BTU_LOGO.png' className='w-full h-full' />
       </div>
+      <div className='flex items-center text-white gap-10'>
+        <div className='border'>
+          <a href=''>
+            TEAM
+          </a>
+        </div>
+        <div className='border'>
+          <a>
+            ORIGINS
+          </a>
+        </div>
+        <div className='border'>
+          <a>
+            BTU ART
+          </a>
+        </div>
+        <div className='border'>
+          <a>
+            NFT GALLERY
+          </a>
+        </div>
+        <div className='border'>
+          <a>
+            THE IP
+          </a>
+        </div>
+        <div className='border'>
+          <a>
+            CONNECT
+          </a>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/app/layout/Main.tsx
+++ src/app/layout/Main.tsx
@@ -52,13 +52,13 @@ function Main() {
   }, [scrollY])
 
   return (
-    <div className='md:sticky top-0 bg-illu_001 bg-cover bg-no-repeat h-screen md:h-[80vw] w-full overflow-y-hidden'>
+    <div className='md:sticky top-0 bg-illu_001 bg-cover bg-no-repeat h-screen md:h-[100vw] w-full overflow-y-hidden'>
 
       <div className='h-screen w-full flex flex-col md:flex-row items-center justify-center px-5 overflow-y-hidden'>
 
         {/* BTU CHARACTERS */}
         <div className='relative w-full flex border-red-500  sm:justify-center md:justify-end h-screen lg:pr-[50px]'>
-          <img src='/assets/Illus001/Illu01CH.png' className='z-30 min-w-[400px] left-[-20%] xs:left-auto sm:w-[500px] lg:w-[600px] bottom-0 absolute' />
+          <img src='/assets/Illus001/Illu01CH.png' className='z-30 min-w-[350px] left-[-20%] xs:left-auto sm:w-[500px] lg:w-[600px] xl:w-[700px] bottom-0 absolute' />
           {/* <img src='/assets/Illus001/BTU_Website_Illus001_Yosuke.png' className='z-30 w-[100vw] min-w-[600px] max-w-[900px] absolute' />
           <img src='/assets/Illus001/BTU_Website_Illus001_Chop.png' className='z-30 w-[100vw] min-w-[600px] max-w-[900px] absolute' /> */}
         </div>
@@ -70,46 +70,47 @@ function Main() {
         {/* BTU BACKGROUND */}
         <img src='/assets/Illus001/LOGOBG.png' className={` hidden md:block btu top-20 sm:top-none absolute lg:w-[80%]`} />
         <img src='/assets/Illus001/LOGOBG.png' className={` absolute top-32 xs:top-20 sm:top-none md:hidden w-[100vw]`} />
-        <div className={`hidden w-[400px] lg:w-[700px] md:absolute left-[2%] tracking-widest md:flex flex-col space-y-2 px-5 relative top-20 sm:top-40`}>
-          <AnimatePresence>
-            {
-              scrollY > 0 && (
-                <>
-                  <motion.p
-                    key="paragraph1"
-                    initial={{ y: 15, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: 15, opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0 }}
-                    className='font-bold text-pink-500 md:text-2xl lg:text-[25px]'>
-                    Beat Them Up is renewing the entertainment industry by creating the first VISUAL, MUSICAL & Social 360 IP!
-                  </motion.p>
-                  <motion.p
-                    key="paragraph2"
-                    initial={{ y: 15, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: 15, opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0.25 }}
-                    className='md:text-xl lg:text-[20px]'
-                  >
-                    BTU is a shonen anime about music but foremost, a story about art and creativity…
-                  </motion.p>
-                  <motion.p
-                    key="paragraph3s"
-                    initial={{ y: 15, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: 15, opacity: 0 }}
-                    transition={{ duration: 0.5, delay: 0.5 }}
-                    className='md:text-xl lg:text-[20px]'
-                  >
-                    So, put a vinyl on your turntable, connect your headphones, turn on the volume and push the Play button…
-                  </motion.p>
+        <div className='w-full absolute h-screen flex items-center'>
+          <div className={`hidden md:w-[500px] lg:w-[600px] xl:w-[800px] md:absolute left-[2%] tracking-widest md:flex flex-col space-y-5 px-5 relative mb-[250px]`}>
+            <AnimatePresence>
+              {
+                scrollY > 0 && (
+                  <>
+                    <motion.p
+                      key="paragraph1"
+                      initial={{ y: 15, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 15, opacity: 0 }}
+                      transition={{ duration: 0.5, delay: 0 }}
+                      className='font-bold text-white md:text-2xl xl:text-[30px]'>
+                      Beat Them Up is renewing the entertainment industry by creating the first VISUAL, MUSICAL & Social 360 IP!
+                    </motion.p>
+                    <motion.p
+                      key="paragraph2"
+                      initial={{ y: 15, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 15, opacity: 0 }}
+                      transition={{ duration: 0.5, delay: 0.25 }}
+                      className='md:text-xl xl:text-[25px] text-white'
+                    >
+                      BTU is a shonen anime about music but foremost, a story about art and creativity…
+                    </motion.p>
+                    <motion.p
+                      key="paragraph3s"
+                      initial={{ y: 15, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 15, opacity: 0 }}
+                      transition={{ duration: 0.5, delay: 0.5 }}
+                      className='md:text-xl xl:text-[25px] text-white'
+                    >
+                      So, put a vinyl on your turntable, connect your headphones, turn on the volume and push the Play button…
+                    </motion.p>
 
-                </>
-              )
-            }
-          </AnimatePresence>
-
+                  </>
+                )
+              }
+            </AnimatePresence>
+          </div>
         </div>
 
 
